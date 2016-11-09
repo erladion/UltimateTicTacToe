a688e89488df1d1c79901518ef08584c8588b5d4
erladion <johan.jansson94@gmail.com>
2016-11-09 10:15:59 +0100

    Lägg till interface

---
+++ UltimateTicTacToe/UltimateTicTacToe/ApplicationTier/SmallBoardInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UltimateTicTacToe.ApplicationTier
+{
+    interface SmallBoardInterface
+    {
+        Mark getMark(Point pt);
+
+        void setMark(Point pt, Mark mark);
+    }
+}
