160063dd53cd94d03e05faac316b00c021851f90
erladion <johan.jansson94@gmail.com>
2016-11-11 11:28:01 +0100

    Added some stuff we forgot RIP Harambe

---
+++ UltimateTicTacToe/UltimateTicTacToe/ApplicationTier/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UltimateTicTacToe.ApplicationTier
+{
+    class Game
+    {
+        private MainBoard mb = null;
+        public Game()
+        {
+
+        }
+
+        public void initGame()
+        {
+            Mark[,] m = new Mark[3,3];
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    m[x, y] = Mark.Empty;
+                }
+            }
+            SmallBoard sb = new SmallBoard(m);
+
+            SmallBoard[,] sba = new SmallBoard[3,3];
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    sba[x,y] = sb;
+                }
+            }
+            this.mb = new MainBoard(sba);
+        }
+
+        public MainBoard MB{
+            get
+            {
+                return this.mb;
+            }
+        }
+    }
+}
